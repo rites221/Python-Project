2b44ae429091589498117f6f0a292aaa052edaf2
rites221 <119439984+rites221@users.noreply.github.com>
2023-07-24 13:47:19 +0530

    Update TreasureIsland.py

--- Treasue Island End/TreasureIsland.py
+++ Treasue Island End/TreasureIsland.py
@@ -21,4 +21,23 @@ if Start_the_game == "left":
 else:
     print("Fall into a hole. Game Over")
 
+#Treasure Island Game Using If and Else Loop
+
+print("Welcome to the treasure island\nYour duty is to find the treasure")
+
+Start_game = input("You are at crossroads. Where do you want to go 'Right' or 'Left'\n").lower()
+if Start_game == 'left':
+    Swim_wait = input('What do you want to do now? Swim or Wait\n').lower()
+    if Swim_wait == 'wait':
+            Blue_Green_Red = input('Choose the door-Red, Blue, Green\n')
+            if Blue_Green_Red == 'red':
+                print("You Won")
+            else:
+                print('Sorry, You lose')
+    else:
+        print('Sorry, You lose')
+
+else:
+    print('Sorry, You lose')
+
 
